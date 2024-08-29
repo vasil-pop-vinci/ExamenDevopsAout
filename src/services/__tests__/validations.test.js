b5de4ee8da96ffc92847993c59e58bc00746eef6
Pop <vasil.pop@student.vinci.be>
2024-08-29 16:46:27 +0200

    add : handle non-leap years divisible by 100 but not by 400

--- src/services/__tests__/validations.test.js
+++ src/services/__tests__/validations.test.js
@@ -13,3 +13,9 @@ describe('Leap Year Validation', () => {
         expect(isValid(2000)).toBe(true);
     });
 });
+
+test('should return false for years divisible by 100 but not by 400', () => {
+    expect(isValid(1900)).toBe(false);
+    expect(isValid(2100)).toBe(false);
+});
+
--- src/services/validations.js
+++ src/services/validations.js
@@ -1,5 +1,11 @@
 const isValid = (year) => {
-    return year % 400 === 0;
+    if (year % 400 === 0) {
+        return true;
+    }
+    if (year % 100 === 0) {
+        return false;
+    }
+    return year % 4 === 0;
 };
 
 exports.isValid = isValid;
